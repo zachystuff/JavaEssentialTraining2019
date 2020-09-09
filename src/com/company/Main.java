6d1d264e9453f45829b1208a5689ee69a51b767f
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-09-09 10:55:36 -0500

    Converting primitives to strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,16 @@ public class Main {
         int result = 10 * 12;
         String answer = "The answer is " + result;
         System.out.println(answer);
+
+        String howMany = 20 + " things";
+        System.out.println(howMany);
+
+        int intValue = 42;
+        var fromInt = Integer.toString(intValue);
+        System.out.println(fromInt);
+
+        boolean boolValue = true;
+        var fromBool = Boolean.toString(boolValue);
+        System.out.println(fromBool);
     }
 }
