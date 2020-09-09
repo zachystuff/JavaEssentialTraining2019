6b7c3419c682a37b0afa8209379c6423098629a1
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-09-09 12:10:33 -0500

    Parse strings!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         var s1 = "Welcome to California!";
+
+        System.out.println("Length of string: " + s1.length());
+
+        var position = s1.indexOf("California");
+        System.out.println("The position is " + position);
+
+        var sub = s1.substring(11);
+        System.out.println(sub);
+
+        String s2 = "Welcome!    ";
+        var len = s2.length();
+        System.out.println(len);
+
+        var trimmed = s2.trim();
+        System.out.println(trimmed.length());
     }
 }
