75a0e26045d5d905811194aa2bf7fd92d8d84708
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-09-09 12:29:31 -0500

    Simple Calculator Application!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        var scanner = new Scanner(System.in);
+
+        System.out.print("Enter a numeric value: ");
+        var number1 = scanner.nextDouble();
+
+        System.out.print("Enter a numeric value: ");
+        var number2 = scanner.nextDouble();
+
+        double sum = number1 / number2;
+        System.out.println("The answer is: " + sum);
+
     }
 }
