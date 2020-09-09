7b536d0a1c432cffb00f6a718aa9b9baf08758f8
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-09-09 12:23:11 -0500

    Using a scanner to get values. Yaay!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.print("Enter a value: ");
+
+        var input = scanner.nextLine();
+        System.out.println(input);
+
+
+        System.out.print("Enter number1: ");
+        var number1 = scanner.nextInt();
+
+        System.out.print("Enter number2: ");
+        var number2 = scanner.nextInt();
+
+        var sum = number1 + number2;
+        System.out.println("The sum is : " + sum);
 
     }
 }
