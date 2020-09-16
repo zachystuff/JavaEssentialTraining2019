5f3f68ffdc1d1682619e1ad7f076e4b994b8180a
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-09-16 09:31:17 -0500

    Looping with Java!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,28 @@ public class Main {
                         "July", "August", "September",
                         "October", "November", "December"};
 
+//        ITERATIVE LOOP
+//        for (int i = 0; i < months.length; i++) {
+//            System.out.println(months[i]);
+//        }
+
+//        FOREACH LOOP
+//        for (String month : months) {
+//            System.out.println(month);
+//        }
+
+//        WHILE LOOP
+//        var whileCounter = 0;
+//        while (whileCounter < months.length) {
+//            System.out.println(months[whileCounter]);
+//            whileCounter++;
+//        }
+
+//        DO LOOP
+        var doCounter = 0;
+        do {
+            System.out.println(months[doCounter]);
+            doCounter++;
+        } while (doCounter < months.length);
     }
 }
