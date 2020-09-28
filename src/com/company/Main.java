f3e0c7acdf03174d22e075d091c958b574d576a0
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-09-28 09:25:19 -0500

    try catch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        String s = null;
+        try {
+            var sub = s.substring(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Im not dead yet");
     }
 }
